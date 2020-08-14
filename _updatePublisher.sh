6490acdc2c4adb3117f39f01293d2a4434e28b09
Tery Tsui <teryfly@qq.com>
2020-08-14 09:42:05 +0800

    Update _updatePublisher.sh
    更换publisher源

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dlurl=https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar
+dlurl=https://github.com/HL7China/fhir-ig-publisher/releases/latest/download/publisher.jar
 publisher_jar=publisher.jar
 input_cache_path=./input-cache/
 
